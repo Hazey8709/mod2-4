246c027bfeaa9b5172194aa5ab0c79c395671a8c
Hazey8709 <jfortin802@gmail.com>
2023-03-09 19:35:30 -0500

    ud

--- services/pokemonServ.test.js
+++ services/pokemonServ.test.js
@@ -2,6 +2,7 @@ const { pokemon, pokemonId } = require("../services/pokemonServ");
 
 jest.mock("../__mocks__/pokemonServ");
 
+//! Using API To Test
 describe("Pokemon Test", () => {
     test("Return 20 pokemon", async () => {
         const results = await pokemon().results;
@@ -9,15 +10,33 @@ describe("Pokemon Test", () => {
         expect(results).toBe(results);
     });
 
-    //! Not working as aspected
+    //! Not working as expected
     test("Return By ID", async () => {
-        const results = await pokemonId().results[0];
+        const results = await pokemonId(1).results;
 
-        //expect(results[0]).toHaveLength(1);
         expect(results.name).toEqual("bulbasaur");
-        expect(results.url).toEqual("https://pokeapi.co/api/v2/pokemon/1/");
     });
 });
+
+// //! To Use MOCK Data to TEST --(NOT API)--
+// describe("Pokemon Test 2", () => {
+//     test("Return 20 pokemon", async () => {
+//         const results = await pokemon().data;
+
+//         expect(results).toBe(results);
+//     });
+
+
+//     test("Return By ID", async () => {
+//         const results = await pokemonId(1);
+
+//         //expect(results.data.id).toHaveLength(1);
+//         expect(results.data.name).toEqual("bulbasaur");
+//         // expect(results.data.url).toEqual(
+//         //     "https://pokeapi.co/api/v2/pokemon/1/"
+//         // );
+//     });
+// });
 
 // describe("", () => {
 //     test("", () => {
